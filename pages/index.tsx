6bba6ec623d825b2074c93ce9458639c68f91f79
Max <mkorobov2002@mail.ru>
2022-11-18 00:52:07 +0300

    Head Tag Added

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,26 @@ import { ChakraProvider, Box, theme } from '@chakra-ui/react';
 import TodoAdd from '@/components/TodoAdd';
 import TodoList from '@/components/TodoList';
 import TopBar from '@/components/TopBar';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <ChakraProvider theme={theme}>
-      <Box maxWidth='8xl' margin='auto' p={5}>
-        <TopBar />
-        <TodoList />
-        <TodoAdd />
-      </Box>
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Todo List Using Next.js, TypeScript, MobX</title>
+        <meta
+          name='description'
+          content='Todo List Using Next.js, TypeScript, MobX'
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Box maxWidth='8xl' margin='auto' p={5}>
+          <TopBar />
+          <TodoList />
+          <TodoAdd />
+        </Box>
+      </ChakraProvider>
+    </>
   );
 }
